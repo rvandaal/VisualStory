d489cf3ddf59d9423572885c148557a8e1e8123e
Rob van Daal <rvandaal@hotmail.com>
2019-03-07 23:24:30 +0100

    First version with UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  @ViewChild('input') input: ElementRef;
+
+  outputChars: string[];
+
+  get inputText() {
+    return this.input.nativeElement.value;
+  }
+
+  private onTransform() {
+    this.outputChars = Array.from('hello world');
+  }
+
+
 }
